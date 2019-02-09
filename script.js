efa38a43f0c658111e06e02fe3102bdefda584bf
khadijat Mukhtar Erena <nanakhadijaterena@gmail.com>
2019-02-09 03:58:47 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var name = "khaijat";
+var age = 17;
+var isMyNameKadijat = true;
+var isUndefined;
+var isNull = null;
+var isObject = {food:"rice, milk"}
+var firstJavascript = "my first JS work";
+console.log(name,age,isMyNameKadijat,isUndefined,isNull,isObject,firstJavascript);
